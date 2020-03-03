2b0ce1a5a196721f63485184dd7e366a9c939429
Gustavo Costa <gustavo_martins1991@hotmail.com>
2020-03-03 11:08:51 -0300

    Criação das entidades

---
+++ QuickBuy/QuickBuy.Domain/Entities/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Domain.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Domain.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+    }
+}
---
+++ QuickBuy/QuickBuy.Domain/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Domain.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartaCredito = 2,
+        Deposito = 3
+
+    }
+}
